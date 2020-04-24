f3fabfa71ec1b97611d8e87d114f5883fda5c752
RJ <RJ@DESKTOP-SPMHGT3>
2020-04-24 10:54:14 -0500

    Updated to version 0.5 - Phase 2 - Added Hashing

--- LMS-Group4/src/Authenticate.java
+++ LMS-Group4/src/Authenticate.java
@@ -1,25 +1,40 @@
+/**
+   [Authentication function to hash passwords for storage.]
+   
+   [Notes]
+   
+   @author [Richard Arnold, Redi Delulo, Krista Burdick, Chris Hammond, Alyssa Knight and Matt Worman]
+   @version $Revision: .5 $ $Date: 2020/24/04 04:23:26 $
+
+**/
+
+//Imports
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Authenticate {
-
+	//Returns the hashed String
 	public static String encrypt(String p, String uid) {
 		try {
+			//Uses SHA-256 for security
 			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			//Combines password and unique ID for added security.
 			String fullString = p + uid;
+			//Hashes the password using SHA-256
 			byte[] hashedByte = digest.digest(fullString.getBytes(StandardCharsets.UTF_8));
 			
+			//Converts the hashed password to a string for storage in SQLite
 			StringBuffer hexString = new StringBuffer();
 			for (int i = 0; i < hashedByte.length; i++) {
 				String hex = Integer.toHexString(0xff & hashedByte[i]);
 			    if(hex.length() == 1) hexString.append('0');
 			        hexString.append(hex);
 			}
+			//Returns the hashed password in string format.
 			return hexString.toString();		
 			
 		} catch (NoSuchAlgorithmException exception) {
-	            // TODO Auto-generated catch block
 	            exception.printStackTrace();
 	    }
 		return "";
